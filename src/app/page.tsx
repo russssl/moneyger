d3cffa821924425a47ce1b73fc609981b27db11e
russssl <70024944+russssl@users.noreply.github.com>
2024-11-11 21:19:56 +0100

    add shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import { auth } from '@/server/auth';
+import Link from 'next/link';
+
+export default async function HomePage() {
+  const session = await auth();
+  return (
+    <>
+      {session ? (
+        <div>
+          <h1>Welcome {session.user.name}</h1>
+          <div>
+            If you want to sign out,{'\n'}
+            <Link href="/api/auth/signout">
+              Sign out
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>Sign in</h1>
+          <p>
+            You are not signed in.{' '}
+            <Link href="/api/auth/signin">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      )}
+    </>
+  );
+}
