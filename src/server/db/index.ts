e541356856def65e8ae2a3b3208be043fbcba224
russssl <deniskalicen@gmail.com>
2025-02-20 23:36:21 +0100

    migrate to neon (attempt)

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
 import * as schema from "./schema";
 
@@ -7,19 +7,13 @@ import * as schema from "./schema";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
+// const globalForDb = globalThis as unknown as {
+//   conn: postgres.Sql | undefined;
+// };
 const env = process.env;
 if (!env) {
   throw new Error("Environment variables not loaded");
 }
-const conn = globalForDb.conn ?? postgres({
-  host: env.POSTGRES_HOST,
-  user: env.POSTGRES_USER,
-  password: env.POSTGRES_PASSWORD,
-  database: env.POSTGRES_DB,
-});
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const sql = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle(conn, { schema });
+export const db = drizzle(sql, { schema });
