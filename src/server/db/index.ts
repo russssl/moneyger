cae716370d404bf33b984109e2f1036f1c43ecf9
russssl <deniskalicen@gmail.com>
2025-02-20 23:24:18 +0100

    revert

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -11,16 +11,15 @@ import * as schema from "./schema";
 const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
-// if (!env) {
-//   throw new Error("Environment variables not loaded");
-// }
-console.log("environment:" + env?.NODE_ENV);
+if (!env) {
+  throw new Error("Environment variables not loaded");
+}
 const conn = globalForDb.conn ?? postgres({
-  host: env!.POSTGRES_HOST,
-  user: env!.POSTGRES_USER,
-  password: env!.POSTGRES_PASSWORD,
-  database: env!.POSTGRES_DB,
+  host: env.POSTGRES_HOST,
+  user: env.POSTGRES_USER,
+  password: env.POSTGRES_PASSWORD,
+  database: env.POSTGRES_DB,
 });
-if (env!.NODE_ENV !== "production") globalForDb.conn = conn;
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
