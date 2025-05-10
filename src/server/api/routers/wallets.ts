182a6faedc7e0cb19b47a996b32011ac81beedbb
russssl <deniskalicen@gmail.com>
2025-05-10 14:41:07 +0200

    improve wallets api

--- src/server/api/routers/wallets.ts
+++ src/server/api/routers/wallets.ts
@@ -3,26 +3,24 @@ import {
   protectedProcedure,
 } from "@/server/api/trpc";
 import { transactions } from "@/server/db/transaction";
-import { wallets } from "@/server/db/schema";
+import { type Wallet, wallets } from "@/server/db/schema";
 import { and, eq } from "drizzle-orm";
 import { z } from "zod";
-import { getFormattedWallets } from "../services/wallets";
 
 export const walletRouter = createTRPCRouter({
   getWallets: protectedProcedure
-    .query(async ({ ctx }) => {
+    .query(async ({ ctx }): Promise<Wallet[]> => {
       const res_wallets = await ctx.db.query.wallets.findMany({
         where: eq(wallets.userId, ctx.session.user.id),
       });
-
-      return await getFormattedWallets(res_wallets);
+      return res_wallets;
     }),
 
   getWalletById: protectedProcedure
     .input(z.object({
       id: z.string().nullable(),
     }))
-    .query(async ({ ctx, input }) => {
+    .query(async ({ ctx, input }): Promise<Wallet> => {
       if (!input.id) {
         throw new Error("Wallet id not provided");
       }
@@ -39,73 +37,119 @@ export const walletRouter = createTRPCRouter({
       if (!res_wallet) {
         throw new Error("Wallet not found");
       }
-      return {
-        name: res_wallet.name,
-        currency: res_wallet.currency,
-        id: res_wallet.id,
-        initialBalance: res_wallet.transactions.reduce((acc: number, curr: {amount: number | null}) => acc + (curr.amount ?? 0), 0),
-      };
+      return res_wallet;
     }),
 
-  updateWallet: protectedProcedure
+  // updateWallet: protectedProcedure
+  //   .input(z.object({
+  //     id: z.string().optional(),
+  //     name: z.string(),
+  //     initialBalance: z.number().optional(),
+  //     currency: z.string(),
+  //   }))
+  //   .mutation(async ({ ctx, input }): Promise<Wallet> => {
+  //     let res_wallet: Wallet;
+  //     let updatedWallet;
+  //     if (input.id) {
+  //       const wallet = await ctx.db.query.wallets.findFirst({
+  //         where: and(
+  //           eq(wallets.userId, ctx.session.user.id),
+  //           eq(wallets.id, input.id),
+  //         ),
+  //       });
+
+  //       if (!wallet) {
+  //         throw new Error("Wallet not found");
+  //       }
+
+  //       // update the wallet
+  //       updatedWallet = await ctx.db.update(wallets).set({
+  //         name: input.name,
+  //         currency: input.currency,
+  //       }).where(
+  //         and(
+  //           eq(wallets.userId, ctx.session.user.id),
+  //           eq(wallets.id, input.id),
+  //         )).returning().execute();
+  //     } else {
+  //       updatedWallet = await ctx.db.insert(wallets).values({
+  //         userId: ctx.session.user.id,
+  //         name: input.name,
+  //         currency: input.currency,
+  //         balance: input.initialBalance ?? 0,
+  //       }).returning().execute().then((res) => res[0]);
+        
+  //       if (!updatedWallet) {
+  //         throw new Error("Wallet not found");
+  //       }
+
+  //       if (input.initialBalance) {
+  //         await ctx.db.insert(transactions).values({
+  //           walletId: res_wallet.id,
+  //           amount: input.initialBalance,
+  //           type: "adjustment",
+  //           userId: ctx.session.user.id,
+  //         }).execute();
+  //       }
+  //     }
+      
+  //   }),
+  
+  createWallet: protectedProcedure
     .input(z.object({
-      id: z.string().optional(),
       name: z.string(),
-      initialBalance: z.number().optional(),
       currency: z.string(),
+      initialBalance: z.number().optional(),
     }))
-    .mutation(async ({ ctx, input }) => {
-      let res_wallet;
-      if (input.id) {
-        const wallet = await ctx.db.query.wallets.findFirst({
-          where: and(
-            eq(wallets.userId, ctx.session.user.id),
-            eq(wallets.id, input.id),
-          ),
-        });
-
-        if (!wallet) {
-          throw new Error("Wallet not found");
-        }
-
-        // update the wallet
-        res_wallet = await ctx.db.update(wallets).set({
-          name: input.name,
-          currency: input.currency,
-        }).where(
-          and(
-            eq(wallets.userId, ctx.session.user.id),
-            eq(wallets.id, input.id),
-          )).returning().execute();
-      } else {
-        res_wallet = await ctx.db.insert(wallets).values({
-          userId: ctx.session.user.id,
-          name: input.name,
-          currency: input.currency,
-        }).returning().execute();
-
-        if (input.initialBalance && res_wallet[0]) {
-          await ctx.db.insert(transactions).values({
-            walletId: res_wallet[0].id,
-            amount: input.initialBalance,
-            type: "adjustment",
-            userId: ctx.session.user.id,
-          }).execute();
-        }
-      }
+    .mutation(async ({ ctx, input }): Promise<Wallet> => {
+      const res_wallet = await ctx.db.insert(wallets).values({
+        userId: ctx.session.user.id,
+        name: input.name,
+        currency: input.currency,
+        balance: input.initialBalance ?? 0,
+      }).returning().execute().then((res) => res[0]);
 
       if (!res_wallet) {
         throw new Error("Wallet not found");
       }
-      return await getFormattedWallets(res_wallet);
+
+      if (input.initialBalance) {
+        await ctx.db.insert(transactions).values({
+          walletId: res_wallet.id,
+          amount: input.initialBalance,
+          type: "adjustment",
+          userId: ctx.session.user.id,
+        }).execute();
+      }
+
+      return res_wallet;
+    }),
+
+  updateWallet: protectedProcedure
+    .input(z.object({
+      id: z.string(),
+      name: z.string(),
+      currency: z.string(),
+    }))
+    .mutation(async ({ ctx, input }): Promise<Wallet> => {
+      const res_wallet = await ctx.db.update(wallets).set({
+        name: input.name,
+        currency: input.currency,
+      }).where(eq(wallets.id, input.id)).returning().execute().then((res) => res[0]);
+
+      if (!res_wallet) {
+        throw new Error("Wallet not found");
+      }
+
+      return res_wallet;
     }),
 
   deleteWallet: protectedProcedure
     .input(z.object({
       id: z.string(),
     }))
-    .mutation(async ({ ctx, input }) => {
-      await ctx.db.transaction(async (tx) => {
+    .mutation(async ({ ctx, input }): Promise<Wallet> => {
+      const res_wallet = await ctx.db.transaction(async (tx) => {
         const wallet = await tx.query.wallets.findFirst({
           where: and(
             eq(wallets.userId, ctx.session.user.id),
@@ -127,5 +171,6 @@ export const walletRouter = createTRPCRouter({
 
         return wallet;
       });
+      return res_wallet;
     }),
 });
--- src/server/api/services/wallets.ts
+++ src/server/api/services/wallets.ts
@@ -1,7 +1,6 @@
 import db from "@/server/db";
 import { type Transaction, transactions as transactionsSchema} from "@/server/db/schema";
 import { and, eq, inArray, lt } from "drizzle-orm";
-import { type NewWallet, type Wallet } from "@/server/db/wallet";
 import { env } from "process";
 import { currencyExchangeRates, type SelectCurrencyExchangeRate } from "@/server/db/currencyExchangeRate";
 import { DateTime } from "luxon";
@@ -17,19 +16,6 @@ export async function calculateWalletBalance(walletId: string) {
   }, 0);
 
   return balance;
-}
-
-export async function getFormattedWallets(wallets: Wallet[] | NewWallet[]) {
-  return await Promise.all(wallets.map(async (wallet: Wallet | NewWallet) => {
-    if (!wallet.id) {
-      throw new Error("wallet id is required");
-    }
-    const balance = await calculateWalletBalance(wallet.id);
-    return {
-      ...wallet,
-      balance,
-    };
-  }));
 }
 
 /**
