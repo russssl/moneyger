c79cd802d3a806a8230470f6c71a15be4d51247e
russssl <deniskalicen@gmail.com>
2025-11-22 01:15:17 +0100

    remove mandatory ssl

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -11,7 +11,7 @@ export default {
     database: env.POSTGRES_DB,
     host: env.POSTGRES_HOST,
     port: Number(env.POSTGRES_PORT) || 5432, // Use environment variable for port with fallback
-    ssl: env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    ssl: env.NODE_ENV === "production" ?  process.env.POSTGRES_SSL === "true" ? { rejectUnauthorized: false } : undefined : undefined,
   },
   tablesFilter: ["manager_v2_*"],
 } satisfies Config;
